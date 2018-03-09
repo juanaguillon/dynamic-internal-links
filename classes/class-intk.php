4b4cc463bc8ae1ddf2c0636bc04743c21d049ae7
miloca98 <miloca.98@hotmail.com>
2018-03-09 13:10:23 -0500

    Inicializacion de Plugin INTK

---
+++ classes/class-intk.php
@@ -0,0 +1,34 @@
+<?php 
+/**
+* @package Interk
+* @version 1.0
+* Creador de la clase base para el plugin Interk
+*/
+class Intk_init_class
+{
+	// Version de plugin.
+	const INTK_VERSION = "1.0";	
+
+	private function get_all_pages( $argum = array() ){
+
+		if ( ! is_array( $argum )){
+			throw new Exception( __('The arguments for pages is not array'));
+			exit;
+		}
+
+		return get_pages( $argum  );
+		
+	}
+
+	public function get_id_page( $args = array() ){
+
+		return $this->get_all_page( $args );
+
+	}
+}
+
+
+
+
+
+ ?>
---
+++ interk.php
@@ -0,0 +1,22 @@
+<?php 
+
+/**
+* 
+* @package Interk
+* @version 1.0
+Plugin Name: Interk
+Description: Create your simple way to generate excellent SEO. Interk will guide you to create your position through connections between pages.
+Author: Juan C
+Version: 1.0
+*/
+
+if( ! defined( 'INTK_PATH')){
+	define( 'INTK_PATH', dirname(__FILE__));
+}
+
+include_once INTK_PATH . '/classes/class-intk.php';
+
+
+
+
+?>
