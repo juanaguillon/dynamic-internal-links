800483d32ddff62cd1a02cc542c8d66b69f4c8ab
miloca98 <miloca.98@hotmail.com>
2018-04-11 10:54:05 -0500

    Manipulacion De Paginas

--- publics/js/admin-settings.js
+++ publics/js/admin-settings.js
@@ -1,7 +1,12 @@
 jQuery(function( $ ){ 
 
-  var sett = new Sets( $('.dyn_page_bd') );
-  sett.mouseMove();
+  var sett = new Sets( $('.dyn_page_bd ') );
+  // sett.mouseMove(  );
+
+  
+  $('.dyn_page_bd').children(':text').change( function( ){
+    sett.move_text( this, parseInt(jQuery( this ).val()) );
+  }); 
   
   
 });
--- publics/js/settings.js
+++ publics/js/settings.js
@@ -1,22 +1,38 @@
 var Sets = function( elements ){ 
 
+  
   this.$el = jQuery(elements);
-
+  
   this.defaults = {    
     clicking: false,
     start: 0,
-    cloneObject: null
-  }
-  var objSets = this;
-
-  this.mouseMove = function ( direction = 'prev' ){    
+    cloneObject: null,
+    elemLength : jQuery( elements ).length,
+    previus: false,
+    nextius: false
     
-    this.$el.mousedown( function(  ){
-      objSets.defaults.clicking = true;
+  }
+  var objSets = this,
+      defaults = objSets.defaults;
 
-      if ( direction == 'prev'){
+  this.mouseMove = function ( direction = 'prev' ){ 
+
+    this.$el.mousedown( function(  ){
+
+      if ( defaults.elemLength < 2 ){
+        return;
+      }      
+      
+      objSets.defaults.clicking = true;
+      
+      var thatEl = jQuery( this );
+      thatEl.prev( ).length > 0 ? defaults.previus = true : false ;
+      thatEl.next( ).length > 0 ? defaults.nextius = true: false;
+
+      if ( direction == 'prev' && defaults.previus ){
         objSets.defaults.start = jQuery(this).prev().position().top;
-      }else if( direction == 'next' ){
+
+      }else if( direction == 'next' && defaults.nextius ){
         objSets.defaults.start = jQuery(this).next().position().top;
       }
 
@@ -25,7 +41,8 @@ var Sets = function( elements ){
     });
 
     jQuery( document ).mouseup( function( ){
-      objSets.defaults.clicking = false;
+
+      defaults.clicking = false;
 
       var cloned = objSets.defaults.clicking;
       if( cloned != null ){
@@ -35,15 +52,10 @@ var Sets = function( elements ){
     });
 
     this.$el.mousemove( function( e ){
-      if ( ! objSets.defaults.clicking ) return false;
 
       
-      var ePage = e.pageY - ( jQuery('#wpbody').offset().top );
-      var starting = objSets.defaults.start;
-      if( starting >= ePage ){
-        objSets.defaults.start = jQuery(this).prev().position().top;
-        console.log( 'box Changed');
-      }
+      if ( ! objSets.defaults.clicking  ) return false; 
+      
       var cloned = objSets.defaults.cloneObject;
       
       cloned.css({
@@ -51,12 +63,38 @@ var Sets = function( elements ){
         "left": ( e.pageX - (jQuery('#wpbody').offset().left ) - (cloned.width() / 2) ) ,
         "top": ( e.pageY - (jQuery('#wpbody').offset().top ) - (cloned.height() / 2) )
         
-      }).removeClass('dyn_page_bd').addClass('bd_clone').prop('disabled',true);
+      }).addClass( 'bd_clone' ).prop('disabled',true);
       jQuery(this).after(cloned);
       
+      console.log( this );
+
+      var ePage = e.pageY - ( jQuery('#wpbody').offset().top ),
+          starting = objSets.defaults.start,
+          dir = direction == 'prev' ? jQuery( this ).prev() : jQuery( this ).next();
+
+      if( starting >= ePage && defaults.previus ){
+        defaults.start = dir.position().top;
+        console.log( 'box Changed');
+      }
       console.log("|ePage| => " + ePage + " and |Starting| => " + starting );
     });    
-  }  
+  } 
+  
+  this.move_text = function( that, int ){
+    if ( isNaN(int) ) return;
+    console.log( 'here');
+    if( this.$el.children( '.val_priority' ) ){
+
+      jQuery(that).parent().prependTo( jQuery('.dynil_setter_pages') );
+
+      
+    }else{
+      
+    }
+
+    
+    
+  }
 }
 
   
